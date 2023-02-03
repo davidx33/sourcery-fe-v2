d78f4301d00739cceae1622751714ec2b1bc8c25
David Xu <david.xu@duke.edu>
2023-02-03 01:20:04 -0500

    feat: changed svg in review these to link to form

--- components/ReviewCompanies.tsx
+++ components/ReviewCompanies.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { ReviewThese } from "@prisma/client";
+import Link from "next/link";
 
 type Props = {
   companies: ReviewThese[];
@@ -20,14 +21,14 @@ const ReviewCompanies: FC<Props> = ({ companies }) => {
               </div >
               <div key={`industry-${index}`}>{company.industry}</div>
               <div key={`url-${index}`} className="flex ml-auto pr-10">
-                <a href={company.url}>
+                <Link href="/dashboard">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path>
                     <path d="M10 14l10 -10"></path>
                     <path d="M15 4l5 0l0 5"></path>
                   </svg>
-                </a>
+                  </Link>
               </div>
             </>
           );
