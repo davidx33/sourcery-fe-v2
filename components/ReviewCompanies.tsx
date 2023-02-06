48e75dfad725cd4cc3fdfde7df215b2f9736bece
David Xu <david.xu@duke.edu>
2023-02-06 13:47:50 -0500

    feat: recommended icon leads to linkedin URL now

--- components/ReviewCompanies.tsx
+++ components/ReviewCompanies.tsx
@@ -21,7 +21,7 @@ const ReviewCompanies: FC<Props> = ({ companies }) => {
               </div >
               <div key={`industry-${index}`}>{company.industry}</div>
               <div key={`url-${index}`} className="flex ml-auto pr-10">
-                <Link href="/dashboard">
+                <Link href={company.url}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path>
