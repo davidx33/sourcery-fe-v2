5517d8b1f367867cb100f12d6c4713d443c110b9
Alan Chen <avchen4@gmail.com>
2023-02-07 17:54:44 -0500

    fix: Have adjusted text sizing when radius is smaller than a certain amount

--- components/ProfilePhoto.tsx
+++ components/ProfilePhoto.tsx
@@ -11,7 +11,7 @@ const ProfilePhoto: FC<Props> = ({ radius, src, name }) => {
 	if (src == null) {
 		return (
 			<div className="flex justify-center">
-				<div style={{ width: `${radius * 2}px`, height: `${radius * 2}px` }} className="mb-3 font-semibold text-3xl bg-indigo-100 text-gray-600 rounded-full ring-4 ring-indigo-400 flex justify-center items-center">
+				<div style={{ width: `${radius * 2}px`, height: `${radius * 2}px` }} className={(radius < 45 ? "" : "text-3xl") + " font-semibold bg-indigo-100 text-gray-600 rounded-full ring-4 ring-indigo-400 flex justify-center items-center"}>
 					{name?.split(' ')[0][0]}{name?.split(' ')[1][0]}
 				</div>
 			</div>
